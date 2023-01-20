4d6e9fa423296f753d878f0a7df5c84a371159bd
Karen Huaulme <karen.huaulme@gmail.com>
2023-01-20 01:04:25 +0100

    ready for lesson 1

--- src/components/Thumb/Thumb.js
+++ src/components/Thumb/Thumb.js
@@ -6,17 +6,6 @@ const Thumb = ({ movie, image, movieID, clickable }) => {
   const score = movie.score.toString().slice(0, 5);
   const plotWithHighlights = buildPlotHighlights(movie.highlights);
   const rating = movie.imdb.rating;
-  const releaseDate = movie.released;
-  let releaseString = "";
-  if (typeof releaseDate === "string") {
-    console.log("STRING!");
-    releaseString = releaseDate.slice(0, 10);
-  }
-
-  let genreString = "";
-  if (movie.genres) {
-    genreString = movie.genres.join(", ");
-  }
 
   return (
     <Wrapper>
@@ -26,8 +15,7 @@ const Thumb = ({ movie, image, movieID, clickable }) => {
         <ScoreBadge>Score: {score}</ScoreBadge>
         <h3>Year: {movie.year}</h3>
         <h3>Rating: {rating}</h3>
-        {releaseString !== "" && <h4>RELEASE DATE: {releaseString}</h4>}
-        <h4 style={{ color: "red" }}>{genreString}</h4>
+
         <h4 dangerouslySetInnerHTML={{ __html: plotWithHighlights }}></h4>
       </Content>
     </Wrapper>
